8cf5e8bbd9731829301141241dfac421e6bfd49d
Tis Lais <christinalais@gmail.com>
2021-03-31 17:39:23 -0700

    add order total to table

--- cart/cart.js
+++ cart/cart.js
@@ -1,14 +1,24 @@
 import { cart } from '../data/cart-data.js';
 import { machines } from '../data/machines-data.js';
-import { createCartRow, findById } from '../utils.js';
+import { createCartRow, findById, calcOrderTotal } from '../utils.js';
 
 const tableBody = document.getElementById('cart-table-body');
+const tableFootTr = document.getElementById('cart-table-foot-tr');
 
 for (let item of cart) {
     const matchingMachine = findById(machines, item.id);
-
     const tr = createCartRow(item, matchingMachine);
-
     tableBody.append(tr);
 }
+
+const total = calcOrderTotal(cart, machines);
+
+const totalTd = document.createElement('td');
+totalTd.classList.add('total-price-td');
+totalTd.textContent = total;
+
+tableFootTr.append(totalTd);
+
+
+
 
