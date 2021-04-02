6beaa118aa63634c791b8004c9d6afd5b4418ad2
Tis Lais <christinalais@gmail.com>
2021-04-02 09:01:57 -0700

    add styling

--- cart/cart.js
+++ cart/cart.js
@@ -1,9 +1,11 @@
-import { cart } from '../data/cart-data.js';
+import { getCart } from '../local-storage-utils.js';
 import { machines } from '../data/machines-data.js';
 import { createCartRow, findById, calcOrderTotal } from '../utils.js';
 
+const cart = getCart();
 const tableBody = document.getElementById('cart-table-body');
 const tableFootTr = document.getElementById('cart-table-foot-tr');
+const button = document.getElementById('place-order');
 
 for (let item of cart) {
     const matchingMachine = findById(machines, item.id);
@@ -19,6 +21,12 @@ totalTd.textContent = `$${total}`;
 
 tableFootTr.append(totalTd);
 
+button.addEventListener('click', () => {
+    const cart = getCart();
+    alert(JSON.stringify(cart));
 
+    localStorage.clear();
+    window.location = '/';
+});
 
 
