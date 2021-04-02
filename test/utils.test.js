1b59133536c09ae6d68ee07a2b6585ccb5ae6fe3
Tis Lais <christinalais@gmail.com>
2021-04-02 14:05:06 -0700

    update DOM test to account for added input element

--- test/utils.test.js
+++ test/utils.test.js
@@ -10,7 +10,7 @@ const test = QUnit.test;
 test('It should take in a machines object and return an li element', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<li class="machine"><h3>Twilight Zone</h3><div class="machine-manu-year"><span class="machine-year">1993</span><span class="machine-manufacturer">Bally Midway</span></div><div class="machine-image" style="background-image: url(../assets/tz1993.jpg);"></div><p>Solid State Electronic</p><p class="machine-description">Flippers (4), Pop bumpers (3), Ramps (2), Dual left inlanes, "Rocket" kicker.</p><p class="machine-price">$250 per month</p><button value="tz1993">Rent</button></li>`;
+    const expected = `<li class="machine"><h3>Twilight Zone</h3><div class="machine-manu-year"><span class="machine-year">1993</span><span class="machine-manufacturer">Bally Midway</span></div><div class="machine-image" style="background-image: url(../assets/tz1993.jpg);"></div><p>Solid State Electronic</p><p class="machine-description">Flippers (4), Pop bumpers (3), Ramps (2), Dual left inlanes, "Rocket" kicker.</p><p class="machine-price">$250 per month</p><input id="input-tz1993" type="number" min="0" max="6" class="machine-amount"><button value="tz1993">Rent</button></li>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
